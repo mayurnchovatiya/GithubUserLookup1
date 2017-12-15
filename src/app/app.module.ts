6a43cf12d762a8bb40738f88f6849b6f83c78d31
Mayur Chovatiya <cmayurchovatiyam@gmail.com>
2017-12-15 14:34:02 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { SearchUserComponent } from './search-user/search-user.component';
-import {UserService} from "./service/user.service";
+import {UserService} from './service/user.service';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import {UserService} from "./service/user.service";
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
