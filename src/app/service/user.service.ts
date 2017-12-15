e9dcf1fa678da4ece362deedc510c5c800e6e2a0
Mayur Chovatiya <cmayurchovatiyam@gmail.com>
2017-12-15 16:08:28 -0500

    .

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from 'rxjs/Observable';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -12,13 +12,13 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
 
-  //Service to retrieve user detail
+  // Service to retrieve user detail
   getUser(name: string): Observable<any> {
     let gitHubUrl = 'https://api.github.com/users/';
     return this.http.get(gitHubUrl + name, httpOptions);
   }
 
-  //Service to retrieve user repo detail
+  // Service to retrieve user repo detail
   getRepo(url: string): Observable<any> {
     return this.http.get(url, httpOptions);
   }
